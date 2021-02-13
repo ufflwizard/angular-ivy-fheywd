978e20a7edcc0ee83c48ff35e13592a49c356383
Emily Rtshiladze <he118017@health.wa.gov.au>
2021-02-13 17:38:27 +0800

    adding example code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CoreModule } from './core/core.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app/app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    // angular
+    BrowserAnimationsModule,
+    BrowserModule,
+
+    // core
+    CoreModule,
+
+    // app
+    AppRoutingModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
